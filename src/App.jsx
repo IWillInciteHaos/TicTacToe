f3011a2864eb6bf5abcb8da55f76cef6de999ab4
Angelina Ivanovic <angelina.gina2@gmail.com>
2023-02-20 16:14:54 +0100

    Start of everything
    it doesn't look nice, work on that later, supposedly it's meant to be ugly for now

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './styles.scss'
+import Board from './components/Board'
+
+
+function App() {
+
+  return (
+    <div className="app">
+      <Board />
+    </div>
+  )
+}
+
+export default App
