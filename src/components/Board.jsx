010fbb1029dd5c9f9976e35fa12ae3b5c268f47f
Angelina Ivanovic <angelina.gina2@gmail.com>
2023-02-21 11:01:11 +0100

    introduced players
    X and O get written intermittently

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,31 +4,35 @@ import Square from "./Square";
 const Board = () => {
 const [squares, setSquares] = useState(Array(9).fill(null));
 console.log(squares);
-  
-const renderSquare = (position) => {
-    return (
-    <Square 
-        value={squares[position]} 
-        onClick = {() => {handleSquareClick(position)}}
-    />
-    );
-}  
+const [isXNext, setIsXNext] = useState(false);
 
 const handleSquareClick = (clickedPosition) => {
+
+    if(squares[clickedPosition])
+    {
+        return;
+    }
+
         setSquares((currentSquares) => {
             return currentSquares.map((squareValue, position) => {
                 if(clickedPosition == position)
                 {
-                    return 'X';
+                    return isXNext ? 'X' : 'O';
                 }
                 return squareValue;
             })
         })
-    }
+        setIsXNext((currentIsXNext) => !currentIsXNext);
+    } 
 
-    
-
-
+    const renderSquare = (position) => {
+        return (
+        <Square 
+            value={squares[position]} 
+            onClick = {() => {handleSquareClick(position)}}
+        />
+        );
+    }  
     
 
 
